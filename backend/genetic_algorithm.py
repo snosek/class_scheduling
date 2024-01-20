5bbef6c91509239f66b43a78d8de0e4ed607ec7f
Stefan Nosek <steph.nosek@gmail.com>
2024-01-20 19:22:09 +0100

    cosmetic changes + drawing parents instead of taking two schedules next to each other

--- backend/genetic_algorithm.py
+++ backend/genetic_algorithm.py
@@ -62,26 +62,23 @@ class Population:
                     for time in [time1, time2]:
                         drawed_classes += [
                             item for item in schedule.schedule[drawed_student_group.id]
-                            if (item.meeting_time.day == time[0] and item.meeting_time.hour == time[1])
+                            if time.__eq__(item.meeting_time)
                         ]
                     drawed_lectures = [item for item in drawed_classes if item.category == "lecture"]
                     drawed_normal = [item for item in drawed_classes if item.category != "lecture"]
                     # najpierw wyklady, coby było git
                     for item in drawed_lectures:
                         other_time = copy([time1, time2])
-                        other_time.remove([item.meeting_time.day, item.meeting_time.hour])
+                        other_time.remove(item.meeting_time)
                         other_time = other_time[0]
                         for group in item.student_groups:
                             this_group_other_class = [
                                 class_ for class_ in schedule.schedule[group.id]
-                                if (
-                                    class_.meeting_time.day == other_time[0]
-                                    and class_.meeting_time.hour == other_time[1]
-                                )
+                                if other_time.__eq__(class_.meeting_time)
                             ]
                             if this_group_other_class:
-                                this_group_other_class[0].set_meeting_time(item.meeting_time.day, item.meeting_time.hour)
-                        item.set_meeting_time(other_time[0], other_time[1])
+                                this_group_other_class[0].set_meeting_time(item.meeting_time)
+                        item.set_meeting_time(other_time)
                         # poniżej break bo wystarczy raz zmienić plan dla każdej grupy jeśli wylosowaliśmy wykład
                         break
                     # poniżej jeszcze jeden break, bo jakbyśmy tego nie zrobili, a wylosowany został jakiś niewykład, to byśmy zmienili plany dla jednej grupy rozwalając wszystko
@@ -90,18 +87,15 @@ class Population:
 
                     for item in drawed_normal:
                         other_time = copy([time1, time2])
-                        other_time.remove([item.meeting_time.day, item.meeting_time.hour])
+                        other_time.remove(item.meeting_time)
                         other_time = other_time[0]
                         drawed_group_other_class = [
                             class_ for class_ in schedule.schedule[drawed_student_group.id]
-                            if (
-                                class_.meeting_time.day == other_time[0]
-                                and class_.meeting_time.hour == other_time[1]
-                            )
+                            if other_time.__eq__(class_.meeting_time)
                         ]
                         if drawed_group_other_class:
-                            drawed_group_other_class[0].set_meeting_time(item.meeting_time.day, item.meeting_time.hour)
-                        item.set_meeting_time(other_time[0], other_time[1])
+                            drawed_group_other_class[0].set_meeting_time(item.meeting_time)
+                        item.set_meeting_time(other_time)
                         # poniżej break bo wystarczy zamiana raz
                         # będąc w tym bloku jesteśmy pewni, że nie wylosowaliśmy wykładu, więc nie ma sytuacji. w której drawed_group_other_class jest wykładem i zmieniamy tylko jego, bez zmiany planów innych grup
                         break
@@ -145,10 +139,14 @@ class Population:
         # elitism
         new_population = copy(self.population[:self.number_of_elites])
         # crossover
+        possible_parents = [*range(0, self.number_of_pairs * 2)]
+        rand.shuffle(possible_parents)
         for i in range(self.number_of_pairs):
-            parents = self.population[2*i : 2*i + 2]
+            drawed_parents = [possible_parents.pop(), possible_parents.pop()]
+            parents = [self.population[drawed_parents[0]], self.population[drawed_parents[1]]]
             children = self.crossover_swap_semesters(parents)
-            self.population[2*i : 2*i + 2] = children
+            self.population[drawed_parents[0]] = children[0]
+            self.population[drawed_parents[1]] = children[1]
         # mutation
         self.mutation_swap_classes(self.population)
         self.mutation_swap_rooms(self.population)
